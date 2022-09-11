78e4edb67fed749f203ee743a36cae8bf5ca799a
Devansh Raut <96239425+devanshraut@users.noreply.github.com>
2022-09-11 21:48:31 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,56 +1,74 @@
- 
+ function soundOn(button) {
+    
+ switch (button) {
+    case "w":
+        var audioCrash = new Audio('sounds/crash.mp3');
+audioCrash.play();
+        break;
+
+        case "a":
+            var audioKick = new Audio('sounds/kick-bass.mp3');
+audioKick.play();
+            break;
+
+                       
+        case "d":
+            var audioSnare = new Audio('sounds/snare.mp3');
+audioSnare.play();
+            break;
+
+               
+            case "s":
+                var audioTom1 = new Audio('sounds/tom-1.mp3');
+audioTom1.play();
+                break;
+    
+               
+                case "j":
+                    var audioTom2 = new Audio('sounds/tom-2.mp3');
+audioTom2.play();
+                    break;
+        
+               
+                    case "k":
+                        var audioTom3 = new Audio('sounds/tom-3.mp3');
+audioTom3.play();
+                        break;
+                                   
+        case "l":
+            var audioTom4 = new Audio('sounds/tom-4.mp3');
+audioTom4.play();
+            break;
+
+                            
+    default:
+        console.log("wrong")
+    break;
+}
+ }
+
+ function buttonAnimation(currentKey){
+    var activeButton= document.querySelector("."+currentKey);
+    activeButton.classList.add('pressed');
+
+    setTimeout(function(){
+    activeButton.classList.remove('pressed');
+
+    },50)
+    
+ }
+
  for(var i=0;i< document.querySelectorAll(".drum").length;i++){
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
  var inner=this.innerHTML;
-    switch (inner) {
-        case "w":
-            var audioCrash = new Audio('sounds/crash.mp3');
-audioCrash.play();
-            break;
-
-            case "a":
-                var audioKick = new Audio('sounds/kick-bass.mp3');
-audioKick.play();
-                break;
-    
-                           
-            case "d":
-                var audioSnare = new Audio('sounds/snare.mp3');
-audioSnare.play();
-                break;
-    
-                   
-                case "s":
-                    var audioTom1 = new Audio('sounds/tom-1.mp3');
-audioTom1.play();
-                    break;
-        
-                   
-                    case "j":
-                        var audioTom2 = new Audio('sounds/tom-2.mp3');
-audioTom2.play();
-                        break;
-            
-                   
-                        case "k":
-                            var audioTom3 = new Audio('sounds/tom-3.mp3');
-audioTom3.play();
-                            break;
-                                       
-            case "l":
-                var audioTom4 = new Audio('sounds/tom-4.mp3');
-audioTom4.play();
-                break;
-    
-                                
-        default:
-            console.log("wrong")
-        break;
-    }
- 
- 
- 
-    }); 
-  
+ soundOn(inner);
+ buttonAnimation(inner);
+ }); 
+  document.addEventListener("keydown",function(event) {
+    var keyName = event.key;
+    var keyName = keyName.toLowerCase();
+   soundOn(keyName);
+   buttonAnimation(keyName);
+  })
    } 
